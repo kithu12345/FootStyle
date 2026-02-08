9bc2e3185b8c590e2200c3382f4be116ea0dbb83
puvanakopis <puvanakopis@gmail.com>
2026-02-08 17:56:29 +0530

    Fix vercel config 1

--- backend/index.js
+++ backend/index.js
@@ -67,5 +67,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running`);
 });
